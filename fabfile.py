555bc17651bbf80d724dddd6e77abed08991fbb1
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2018-06-07 15:03:12 +0200

    Add mpi_install and send_key functions.

--- fabfile.py
+++ fabfile.py
@@ -8,7 +8,7 @@ import backoff
 
 class Job:
     auto_oardel = False
-    hide = True
+    hide = False
 
     def __init__(self, jobid, connection):
         self.jobid = jobid
@@ -45,6 +45,7 @@ class Job:
 
     @backoff.on_exception(backoff.expo, invoke.UnexpectedExit)
     def kadeploy(self, env='debian9-x64-min'):
+        self.hostnames  # Wait for the oar_node_file to be available. Not required, just aesthetic.
         self.connection.run('kadeploy3 -k -f %s -e %s' % (self.oar_node_file, env), hide=self.hide, echo=True)
         return self
 
@@ -52,7 +53,7 @@ class Job:
         return '%s(%d)' % (self.__class__.__name__, self.jobid)
 
     @classmethod
-    def oarsub(cls, constraint, nb_nodes, walltime, connection):
+    def oarsub(cls, connection, constraint, walltime, nb_nodes):
         date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         constraint = '%s/nodes=%d,walltime=%d' % (constraint, nb_nodes, walltime)
         cmd = 'oarsub -t deploy -l "%s" -r "%s"' % (constraint, date)
@@ -60,6 +61,26 @@ class Job:
         regex = re.compile('OAR_JOB_ID=(\d+)')
         jobid = int(regex.search(result.stdout).groups()[0])
         return cls(jobid, connection=connection)
+
+    @classmethod
+    def oarsub_cluster(cls, site, username, clusters, walltime, nb_nodes):
+        connection = cls.g5k_connection(site, username)
+        clusters = ["'%s'" % clus for clus in clusters]
+        constraint = "{cluster in (%s)}" % ', '.join(clusters)
+        return cls.oarsub(connection, constraint, walltime, nb_nodes)
+
+    @classmethod
+    def oarsub_hostnames(cls, site, username, hostnames, walltime, nb_nodes=None):
+        def expandg5k(host, site):
+            if 'grid5000' not in host:
+                host = '%s.%s.grid5000.fr' % (host, site)
+            return host
+        connection = cls.g5k_connection(site, username)
+        hostnames = ["'%s'" % expandg5k(host, site) for host in hostnames]
+        constraint = "{network_address in (%s)}" % ', '.join(hostnames)
+        if nb_nodes is None:
+            nb_nodes = len(hostnames)
+        return cls.oarsub(connection, constraint, walltime, nb_nodes)
 
     @classmethod
     def g5k_connection(cls, site, username):
@@ -77,7 +98,7 @@ class Job:
             self.__nodes = connections
             return self.__nodes
 
-    def apt_install(self, packages):
+    def apt_install(self, *packages):
         self.nodes.run('apt update && apt upgrade -y', hide=self.hide, echo=True)
         cmd = 'apt install -y %s' % ' '.join(packages)
         self.nodes.run(cmd, hide=self.hide, echo=True)
@@ -85,3 +106,33 @@ class Job:
 
 
 #    cmd = 'oarsub -t deploy -l "{cluster in ('nova')}/nodes=1,walltime=4" -r "$(date '+%Y-%m-%d %H:%M:%S')"
+
+def mpi_install(host1, host2, site, username):
+    job = Job.oarsub_hostnames(site, username, hostnames=[host1, host2], walltime=2)
+    print(job)
+    job.kadeploy().apt_install(
+        'build-essential',
+        'python3',
+        'python3-dev',
+        'zip',
+        'make',
+        'git',
+        'time',
+        'libopenmpi-dev',
+        'openmpi-bin',
+        'libxml2',
+        'libxml2-dev',
+    )
+    job.nodes.run('git clone https://gitlab.inria.fr/simgrid/platform-calibration.git', hide=job.hide, echo=True)
+    job.nodes.run('cd platform-calibration/src/calibration && make', hide=job.hide, echo=True)
+    return job
+
+
+def send_key(job):
+    origin = job.nodes[0]
+    target = job.nodes[1]
+    origin.run('ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ""', hide=job.hide, echo=True)
+    origin.get('/root/.ssh/id_rsa.pub', '/tmp/id_rsa.pub')
+    target.put('/tmp/id_rsa.pub', '/tmp/id_rsa.pub')
+    target.run('cat /tmp/id_rsa.pub >> ~/.ssh/authorized_keys', hide=job.hide, echo=True)
+    origin.run('ssh -o "StrictHostKeyChecking no" %s hostname' % target.host, hide=job.hide, echo=True)
