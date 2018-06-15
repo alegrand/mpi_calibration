9360ad01fa2d8e267c81dc0ac76c9b3711070452
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2018-06-15 14:10:35 +0200

    Some exception handling.

--- runner.py
+++ runner.py
@@ -1,6 +1,7 @@
 import fabfile
 import random
 import itertools
+import invoke
 
 
 random.seed(42)
@@ -13,14 +14,17 @@ def run_all(username, site, cluster, possible_node_id, nb_runs=None):
     choices = random.sample(combinations, nb_runs)
     for i, (node1, node2) in enumerate(choices):
         job = 'foo'
-        job = fabfile.Job.oarsub_hostnames(
-            site='lyon',
-            username='tocornebize',
-            hostnames=[node1, node2],
-            walltime=fabfile.Time(minutes=15),
-            immediate=False,
-            script='python3 fabfile.py lyon tocornebize jobid $OAR_JOB_ID')
-        fabfile.logger.info('[%3d/%3d]\t %s: %s and %s' % (i+1, nb_runs, job, node1, node2))
+        try:
+            job = fabfile.Job.oarsub_hostnames(
+                site='lyon',
+                username='tocornebize',
+                hostnames=[node1, node2],
+                walltime=fabfile.Time(minutes=15),
+                immediate=False,
+                script='python3 fabfile.py lyon tocornebize jobid $OAR_JOB_ID')
+            fabfile.logger.info('[%3d/%3d]\t %s: %s and %s' % (i+1, nb_runs, job, node1, node2))
+        except invoke.exceptions.UnexpectedExit:
+            fabfile.logger.warning('oarsub failed for nodes %s and %s' % (node1, node2))
 
 
 run_all('tocornebize', 'lyon', 'nova', range(8))
