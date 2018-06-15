e07f9e0f7022c662517759734a52433c8a5407e4
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2018-06-15 14:21:53 +0200

    Count the node failures.

--- runner.py
+++ runner.py
@@ -1,6 +1,7 @@
 import fabfile
 import random
 import itertools
+import collections
 import invoke
 
 
@@ -12,8 +13,9 @@ def run_all(username, site, cluster, possible_node_id, nb_runs=None):
     combinations = list(itertools.combinations(possible_nodes, 2))
     nb_runs = nb_runs or len(combinations)
     choices = random.sample(combinations, nb_runs)
+    failure_count = 0
+    node_failure_count = collections.Counter()
     for i, (node1, node2) in enumerate(choices):
-        job = 'foo'
         try:
             job = fabfile.Job.oarsub_hostnames(
                 site='lyon',
@@ -25,6 +27,13 @@ def run_all(username, site, cluster, possible_node_id, nb_runs=None):
             fabfile.logger.info('[%3d/%3d]\t %s: %s and %s' % (i+1, nb_runs, job, node1, node2))
         except invoke.exceptions.UnexpectedExit:
             fabfile.logger.warning('oarsub failed for nodes %s and %s' % (node1, node2))
+            failure_count += 1
+            node_failure_count[node1] += 1
+            node_failure_count[node2] += 1
+    fabfile.logger.warning('oarsub failed %d times' % failure_count)
+    fabfile.logger.warning('failure count per node:')
+    for node, nb_failures in sorted(node_failure_count.items()):
+        fabfile.logger.warning('    node %s failed %d times' % (node, nb_failures))
 
 
 run_all('tocornebize', 'lyon', 'nova', range(8))
