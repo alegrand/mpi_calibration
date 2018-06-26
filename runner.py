0566ed433c58fbd3e9523e1a843d1124c9a3810c
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2018-06-26 13:26:14 +0200

    Fix the runner.

--- runner.py
+++ runner.py
@@ -8,7 +8,7 @@ import invoke
 random.seed(42)
 
 
-def run_all(username, site, cluster, possible_node_id, nb_runs=None):
+def run_all(username, site, cluster, possible_node_id, nb_runs=None, deploy=True):
     possible_nodes = ['%s-%d' % (cluster, node_id) for node_id in possible_node_id]
     combinations = list(itertools.combinations(possible_nodes, 2))
     nb_runs = nb_runs or len(combinations)
@@ -16,17 +16,20 @@ def run_all(username, site, cluster, possible_node_id, nb_runs=None):
     failure_count = 0
     node_failure_count = collections.Counter()
     node_tentative_count = collections.Counter()
+    deploy_str = '--deploy ' if deploy else ''
+    script = 'python3 fabfile.py %srennes tocornebize jobid $OAR_JOB_ID' % deploy_str
     for i, (node1, node2) in enumerate(choices):
         node_tentative_count[node1] += 1
         node_tentative_count[node2] += 1
         try:
             job = fabfile.Job.oarsub_hostnames(
-                site='rennes',
-                username='tocornebize',
+                site=site,
+                username=username,
                 hostnames=[node1, node2],
                 walltime=fabfile.Time(minutes=15),
                 immediate=False,
-                script='python3 fabfile.py rennes tocornebize jobid $OAR_JOB_ID')
+                script=script,
+                deploy=deploy)
             fabfile.logger.info('[%3d/%3d]\t %s: %s and %s' % (i+1, nb_runs, job, node1, node2))
         except invoke.exceptions.UnexpectedExit:
             fabfile.logger.warning('oarsub failed for nodes %s and %s' % (node1, node2))
@@ -42,4 +45,4 @@ def run_all(username, site, cluster, possible_node_id, nb_runs=None):
             fabfile.logger.error('Node %s failed every time, consider removing it from your test.' % node)
 
 
-run_all('tocornebize', 'rennes', 'paravance', range(1, 73), 30)
+run_all('tocornebize', 'lyon', 'taurus', range(1, 20), 10, deploy=False)
